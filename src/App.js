f0d296e4a0cfbdcda1a91245303e416e489e2141
171201 <jinalshah5358@gmail.com>
2024-10-22 16:02:34 +0530

    22-10-24

--- src/App.js
+++ src/App.js
@@ -7,12 +7,36 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    console.log(id);
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function onClearList() {
+    setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackagingList />
-      <Stats />
+      <Form addItem={handleAddItems} />
+      <PackagingList
+        setItem={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleChange={handleToggleItem}
+        onClearList={onClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -20,7 +44,7 @@ export default function App() {
 function Logo() {
   return <h1> 🌴 FAR AWAY 💼</h1>;
 }
-function Form() {
+function Form({ addItem }) {
   const [description, setDescription] = useState("");
   const [numItem, setNumItem] = useState(1);
 
@@ -32,6 +56,7 @@ function Form() {
       packed: false,
       id: Date.now(),
     };
+    addItem(newItem);
     console.log(newItem);
 
     setNumItem(1);
@@ -67,32 +92,82 @@ function Form() {
     </form>
   );
 }
-function PackagingList() {
+function PackagingList({ setItem, onDeleteItem, onToggleChange, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItem;
+  if (sortBy === "input") {
+    sortedItem = setItem;
+  }
+  if (sortBy === "description") {
+    sortedItem = setItem
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === "Packed Item") {
+    sortedItem = setItem
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {sortedItem.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleChange={onToggleChange}
+            key={item.id}
+          />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">sort By input value</option>
+          <option value="description">sort By description</option>
+          <option value="Packed Item">sort By Packed Item</option>
+        </select>
+        <button onClick={() => onClearList()}>Clear List</button>
+      </div>
     </div>
   );
 }
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleChange }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleChange(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  console.log(!items.length);
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding your items to your packing List 🚀</em>
+      </footer>
+    );
+  }
+  const numItems = items.length;
+  const packedItem = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItem / numItems) * 100);
+  console.log(packedItem);
   return (
     <footer className="stats">
-      <em>Footer</em>
+      <em>
+        {percentage === 100
+          ? "You got everything ! Ready to Go ✈️"
+          : `💼 you have ${numItems} items on your list, and you already packed ${packedItem} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
